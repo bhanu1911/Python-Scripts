1e9246dd400d9daabf3b229b9dea93de6549b8a2
Bhanu Bhandari <bhanubhandari99@gmail.com>
2017-04-12 18:42:54 +0530

    Final update to the script

--- AutoRetrieve/script_ar.py
+++ AutoRetrieve/script_ar.py
@@ -3,11 +3,11 @@ import requests
 import json
 import codecs
 import random
-t=0
+i=0
 list1 = {}
-print("In the mood for some jokes, are we?"
-for t in range (1,12):
-    url = "http://123hindijokes.com/very-funny-jokes/"+str(j)
+print("In the mood for some jokes, are we?")
+for t in range(1,12):
+    url = "http://123hindijokes.com/very-funny-jokes/"+str(t)
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "lxml")
     uls = soup.findAll('ul',{'class':'statusList'})
@@ -15,6 +15,6 @@ for t in range (1,12):
         for li in ul.findAll('li'):
             list1[i]= li.get_text()
             i=i+1
-with open('a.json', 'w', encoding='utf-8') as outfile:
+with codecs.open('a.json', 'w', 'utf-8') as outfile:
     outfile.write(json.dumps(list1, ensure_ascii=False,indent = 4))
 print("Here you go!")
