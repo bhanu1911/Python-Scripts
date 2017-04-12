6a656d2350b3113a797424f440d0b66542728aa4
Bhanu Bhandari <bhanubhandari99@gmail.com>
2017-04-12 18:22:59 +0530

    Minor update

--- AutoRetrieve/script_ar.py
+++ AutoRetrieve/script_ar.py
@@ -1,12 +1,22 @@
+import codecs
+import random
+i=0
+lis = {}
 import requests
 import time
 from bs4 import BeautifulSoup
-def write1():
-   var="file.json"
-   k=open(var)
-   r = requests.get('http://123hindijokes.com/very-funny-jokes')
-   soup = BeautifulSoup(r.content, "lxml")
-   x = soup.find_all("li",limit=1)
-   k.write(x)
-time.sleep(1000)
-write1()
+
+for j in range (1,12):
+   time.
+    url="http://123hindijokes.com/very-funny-jokes/"+str(j)
+    r=requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    uls=soup.findAll('ul',{'class':'statusList'})
+    for ul in uls:
+        for li in ul.findAll('li'):
+            lis[i]= li.get_text()
+            i=i+1
+  
+with open('a.json', 'w', encoding='utf-8') as outfile:
+        outfile.write(json.dumps(lis, ensure_ascii=False,indent = 4))
+
